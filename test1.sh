dc4d1e0b4785bf29f2a79ad1f347fa057e1fa4d2
Srijon Saha <srijon365@gmail.com>
2020-03-30 12:50:46 -0700

    Add test cases

--- test1.sh
+++ test1.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+echo "Test 1"
 ./cmd begin localhost 1111 localhost 3333 1001
 ./cmd newa localhost 1111 5
 ./cmd newb localhost 1111 3
--- test2.sh
+++ test2.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
-echo "compiling"
+echo "Test 2"
 ./tworker "5053" "5059" & p1=$!
 ./tworker "5023" "5399" & p2=$!
 ./tmanager "9871" & p3=$!
-wait $p1 $p2 $p3
+sleep 5
 ./cmd begin localhost 5053 localhost 9871 1001
 ./cmd newa localhost 5053 5
 ./cmd newb localhost 5053 3
 ./cmd join localhost 5023 localhost 9871 1001
 ./cmd newa localhost 5023 12
 ./cmd newb localhost 5023 23
-killall -9 $p3
+kill $p3
+wait $p3
 ./cmd commit localhost 5053
-./tmanager "9871" & p4 = $!
-wait $p1 $p2 $p4 $p3
-killall -9 $p1
-killall -9 $p2
-killall -9 $p4
+./tmanager "9871" & p4=$!
+sleep 5
+kill $p1
+wait $p1
+kill $p2
+wait $p2
+kill $p4
+wait $p4
