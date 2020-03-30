33192b75da14db6c1fdf032dcbee6599d728336e
Emma Paris <eparis@atlassian.com>
2020-03-29 19:34:14 -0700

    test 2 script

---
+++ test2.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo "compiling"
+./tworker "6034" "6035" &
+p1=$!
+./tworker "6053" "6059" &
+p2=$!
+./tmanager "4023" &
+p3=$!
+wait $p1 $p2 $p3
+./cmd begin localhost 6034 localhost 4023 1001
+./cmd newa localhost 6034 5
+./cmd newb localhost 6034 3
+./cmd join localhost 6053 localhost 4023 1001 
+./cmd newa localhost 6053 12
+./cmd newb localhost 6053 23
+kill -9 $p3
+./cmd commit localhost 6034
+./tmanager "4023" &
+p4 = $!
+wait $p1 $p2 $p4
