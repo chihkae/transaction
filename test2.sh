e8d54b8b7b2393c46e821d3f44e790b246aa1cf1
Emma Paris <eparis@atlassian.com>
2020-03-30 14:56:20 -0700

    fixed merge conflict

--- test2.sh
+++ test2.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
-echo "Test 2"
-./tworker "5053" "5059" & p1=$!
-./tworker "5023" "5399" & p2=$!
-./tmanager "9871" & p3=$!
-sleep 5
-./cmd begin localhost 5053 localhost 9871 1001
-./cmd newa localhost 5053 5
-./cmd newb localhost 5053 3
-./cmd join localhost 5023 localhost 9871 1001
-./cmd newa localhost 5023 12
-./cmd newb localhost 5023 23
-kill $p3
-wait $p3
-./cmd commit localhost 5053
-./tmanager "9871" & p4=$!
-sleep 5
-kill $p1
-wait $p1
-kill $p2
-wait $p2
-kill $p4
-wait $p4
+echo "compiling"
+./tworker "6099" "5003" & p1=$!
+./tworker "8888" "5004" & p2=$!
+./tmanager "2343" & p3=$!
+wait $p1 $p2 $p3
+./cmd begin localhost 6099 localhost 2343 1001
+./cmd newa localhost 6099 5
+./cmd newb localhost 6099 3
+./cmd join localhost 8888 localhost 2343 1001
+./cmd newa localhost 8888 12
+./cmd newb localhost 8888 23
+killall -9 $p3
+./cmd commit localhost 6099
+./tmanager "2343" & p4 = $!
+wait $p1 $p2 $p4
+killall -9 $p1
+killall -9 $p2
+killall -9 $p4
