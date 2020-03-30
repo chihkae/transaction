a9cff28c6f0d9237adafacb7fc67f1e527e3f1bf
Srijon Saha <srijon365@gmail.com>
2020-03-29 20:06:26 -0700

    Remove kill

--- test2.sh
+++ test2.sh
@@ -10,7 +10,7 @@ wait $p1 $p2 $p3
 ./cmd begin localhost 6034 localhost 4023 1001
 ./cmd newa localhost 6034 5
 ./cmd newb localhost 6034 3
-./cmd join localhost 6053 localhost 4023 1001 
+./cmd join localhost 6053 localhost 4023 1001
 ./cmd newa localhost 6053 12
 ./cmd newb localhost 6053 23
 killall -9 $p3
@@ -20,5 +20,4 @@ p4 = $!
 wait $p1 $p2 $p4 $p3
 killall -9 $p1
 killall -9 $p2
-killall -9 $p3
 killall -9 $p4
