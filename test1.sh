6167f487f90280b608d9211fbe007e0ff428a6ca
Srijon Saha <srijon365@gmail.com>
2020-03-30 21:53:58 -0700

    Test cases

--- test1.sh
+++ test1.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 echo "Test 1"
-./tworker "1111" "5059" & p1=$!
-./tworker "7777" "5399" & p2=$!
-./tmanager "3333" & p3=$!
+./tworker "1112" "5059" & p1=$!
+./tworker "7778" "5399" & p2=$!
+./tmanager "3334" & p3=$!
 sleep 5
-./cmd begin localhost 1111 localhost 3333 1001
-./cmd newa localhost 1111 5
-./cmd newb localhost 1111 3
-./cmd join localhost 7777 localhost 3333 1001
-./cmd newa localhost 7777 12
-./cmd newb localhost 7777 23
+./cmd begin localhost 1112 localhost 3334 1001
+./cmd newa localhost 1112 5
+./cmd newb localhost 1112 3
+./cmd join localhost 7778 localhost 3334 1001
+./cmd newa localhost 7778 12
+./cmd newb localhost 7778 23
 sleep 4
-./cmd commit localhost 1111
+./cmd commit localhost 1112
 sleep 4
 kill $p1
 wait $p1
